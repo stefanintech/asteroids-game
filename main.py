e8643662c5255a86d566fac3dd9946c05beaf299
Stefan Taitano <stefantaitano@gmail.com>
2026-03-22 22:05:30 -0500

    asteroids now spawn in game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -11,10 +12,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
